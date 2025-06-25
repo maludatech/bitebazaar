2ca4b24c03ffadfcdda68cb64ec27b8410fb5fde
maluda <nwitevictor7@gmail.com>
2025-06-25 18:59:37 +0100

    update Open graph details

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -31,6 +31,30 @@ export const metadata = {
       href: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Pacifico&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lato&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap",
     },
   ],
+  openGraph: {
+    title: "BiteBazaar",
+    description:
+      "BiteBazaar is your ultimate destination for fresh and delicious meals delivered right to your doorstep.",
+    url: "https://bitebazaer.vercel.app",
+    siteName: "Bitebazaar",
+    images: [
+      {
+        url: "/assets/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Bitebazaar Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "Bitebazaar",
+    title: "Bitebazaar",
+    description:
+      "BiteBazaar is your ultimate destination for fresh and delicious meals delivered right to your doorstep.",
+    images: ["/assets/images/og-image.png"],
+  },
 };
 
 export default function RootLayout({
